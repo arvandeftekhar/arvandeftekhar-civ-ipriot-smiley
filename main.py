a375a645bddf3d9f9ccfd6ea3877e024dd86a11c
Arvand Eftekhar <arvand.eftekhar@gmail.com>
2024-05-19 12:35:36 +0800

    Assessment

--- main.py
+++ main.py
@@ -1,39 +1,22 @@
-""""Demonstrates the use of the Smiley class and its subclasses.
-If you have access to a SenseHAT (either via a Raspberry Pi or a SenseHAT emulator), you can use the real SenseHAT class instead of the mock SenseHAT class.
-That is, delete the sense_hat.py file that is included in this bundle.
-"""
-
 import time
 from happy import Happy
 from sad import Sad
 
 if __name__ == '__main__':
-    # Displaying the Happy Smiley (Yellow)
     happy_smiley = Happy()
     print("Displaying Happy Smiley (Yellow):")
     happy_smiley.show()
     time.sleep(1)
-    
-    # Making the Happy Smiley Blink
-    print("Making Happy Smiley Blink:")
-    happy_smiley.blink(delay=0.5)
-    time.sleep(1)
 
-    # Displaying the Sad Smiley (Blue)
     sad_smiley = Sad()
     print("Displaying Sad Smiley (Blue):")
     sad_smiley.show()
     time.sleep(1)
-    
-    # Making the Sad Smiley Blink
+
     print("Making Sad Smiley Blink:")
     sad_smiley.blink(delay=0.5)
     time.sleep(1)
 
-    # Demonstrating polymorphism with the show method
-    smiley = Happy()
-    smiley.show()
+    print("Making Happy Smiley Blink:")
+    happy_smiley.blink(delay=0.5)
     time.sleep(1)
-    
-    # Demonstrating polymorphism with the blink method
-    smiley.blink(delay=0.5)
--- sad.py
+++ sad.py
@@ -15,3 +15,10 @@ class Sad(Smiley):
         eyes = [10, 13, 18, 21]
         for pixel in eyes:
             self.pixels[pixel] = self.BLANK if wide_open else self.complexion()
+
+    def blink(self, delay=0.25):
+        self.draw_eyes(wide_open=False)
+        self.show()
+        time.sleep(delay)
+        self.draw_eyes(wide_open=True)
+        self.show()
